5889746aa551cdbb37c8c3b529ca639e695ca623
Chandana Mahajan <39641650+chandana105@users.noreply.github.com>
2024-02-16 11:15:57 +0530

    feat(search functionality understand, debouncing , fetching search suggestions)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,3 +19,6 @@ export const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 export const YOUTUBE_FETCH_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${YOUTUBE_API_KEY}`;
 
 export const YOUTUBE_FETCH_VIDEO_DETAILS = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=${YOUTUBE_API_KEY}`;
+
+export const YOUTUBE_SUGGESTIONS_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
